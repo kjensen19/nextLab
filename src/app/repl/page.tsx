0f66d2d0fb781c63bed665f13682ad094f9000d9
Kyle Jensen <kjensen19@gmail.com>
2023-02-13 21:49:28 -0600

    skeleton for input component repl

--- src/app/repl/page.tsx
+++ src/app/repl/page.tsx
@@ -1,3 +1,9 @@
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+      <div>
+        <h1 className="text-red-800/25">Hello, replicant</h1>
+        <button className="">Begin</button>
+        <textarea className="caret-pink-500"></textarea>
+      </div>
+      )
   }
