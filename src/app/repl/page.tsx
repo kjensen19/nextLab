44907bcf7b11228387a9c44bd37adc29d1ada265
Kyle Jensen <kjensen19@gmail.com>
2023-02-13 22:30:19 -0600

    styling, border issue resolved

--- src/app/repl/page.tsx
+++ src/app/repl/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
     return (
-      <div>
+      <div className="flex flex-col w-60 align-middle justify-center place-items-center gap-6">
         <h1 className="text-white" >Hello, replicant</h1>
-        <button className="rounded-full mx-8 bg-red-400 px-4 py-1 border-white border-5 border-solid" >Begin</button>
-        <textarea ></textarea>
+        <button className="rounded-full mx-8 bg-red-400 px-4 py-1 border-white border-2 border-solid" >Begin</button>
+        <textarea className="h-40" placeholder=">"></textarea>
       </div>
       )
   }
