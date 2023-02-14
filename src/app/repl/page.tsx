b4b29c2436063065bf41ac3af101a25971f03257
Kyle Jensen <kjensen19@gmail.com>
2023-02-13 22:19:09 -0600

    basic css on button, issue with border needs to be resolved

--- src/app/repl/page.tsx
+++ src/app/repl/page.tsx
@@ -1,9 +1,9 @@
 export default function Page() {
     return (
       <div>
-        <h1 className="text-red-800/25">Hello, replicant</h1>
-        <button className="">Begin</button>
-        <textarea className="caret-pink-500"></textarea>
+        <h1 className="text-white" >Hello, replicant</h1>
+        <button className="rounded-full mx-8 bg-red-400 px-4 py-1 border-white border-5 border-solid" >Begin</button>
+        <textarea ></textarea>
       </div>
       )
   }
