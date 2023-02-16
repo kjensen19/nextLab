7d04267b827ded9d2912cd23c17cc538ba7e3d61
Kyle Jensen <kjensen19@gmail.com>
2023-02-15 20:32:47 -0600

    button handler example

--- src/app/repl/page.tsx
+++ src/app/repl/page.tsx
@@ -3,6 +3,13 @@ export default function Page() {
   // function repl(fn: startRepl) {
   //   //...
   // }
+
+    const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+      event.preventDefault();
+
+      const button: HTMLButtonElement = event.currentTarget;
+    
+  };
   function startRepl(str: string){
     return "replicated"
   }
@@ -11,7 +18,7 @@ export default function Page() {
     return (
       <div className="flex flex-col w-60 align-middle justify-center place-items-center gap-6">
         <h1 className="text-white" >Hello, replicant</h1>
-        <button className="rounded-full mx-8 bg-red-400 px-4 py-1 border-white border-2 border-solid" >Begin</button>
+        <button className="rounded-full mx-8 bg-red-400 px-4 py-1 border-white border-2 border-solid button" onClick={buttonHandler}>Begin</button>
         <textarea className="h-40" placeholder=">"></textarea>
       </div>
       )
